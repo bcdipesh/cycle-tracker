f818e4d3d70e3801a47ea5eae41a58352ba9c60f
Dipesh B C <bcdipeshwork@gmail.com>
2024-11-26 09:39:14 -0500

    Update styles

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ const APP_STATIC_RESOURCES = [
     "/app.js",
     "/style.css",
     "/icons/wheel.svg",
+    "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 ];
 
 // On install, cache the static resources
